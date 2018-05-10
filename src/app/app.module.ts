863957e6b9996d66d627eefd109f26f1777ce147
TheCodeSleyer <ofir3322@gmail.com>
2018-05-10 18:51:43 +0300

    remove dev.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { EnvironmentsModule } from '../environments/environment.module';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    EnvironmentsModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
