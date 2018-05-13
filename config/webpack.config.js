86e1a8f0aed84f6e28fb269bd469febdf74ef951
TheCodeSleyer <ofir3322@gmail.com>
2018-05-14 00:22:01 +0300

    fix Procfile script

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,11 +2,6 @@ const path = require('path');
 const webpack = require('webpack');
 const ionicWebpackFactory = require(process.env.IONIC_WEBPACK_FACTORY);
 
-const appScriptsDir = process.env.IONIC_APP_SCRIPTS_DIR || '@ionic/app-scripts';
-const rootDir = process.env.IONIC_ROOT_DIR;
-
-var config = require(path.join(appScriptsDir, 'config', 'webpack.config.js'));
-
 const resolvePathToEnvModule = (ionicEnv) => {
   let curEnvPath;
   if(ionicEnv == 'prod') {
@@ -17,23 +12,51 @@ const resolvePathToEnvModule = (ionicEnv) => {
   return curEnvPath;
 }
 
+const recoverFailedIonicEnv = (_ionicEnv) => {
+  let ionicEnv;
+  if(process.argv.slice(2).some((arg) => arg.indexOf('--prod') !== -1)) {  
+     // if the build command contains --prod parameter runover the old value of ionicEnv and set to 'prod'.
+    ionicEnv = 'prod';
+
+  } else if(process.env.NODE_ENV == 'production') {
+    // if value of NODE_ENV is 'production' the value on IONIC_ENV can be overridden.
+    ionicEnv = 'prod';
+
+  } else{
+    if(_ionicEnv) {  
+      ionicEnv = _ionicEnv;
+
+    } else {
+      // if ionicEnv undefine set ionicEnv to 'dev'.
+      ionicEnv = 'dev';
+
+    } 
+  }
+  
+  return ionicEnv;
+}
+
+
+const appScriptsDir = process.env.IONIC_APP_SCRIPTS_DIR || '@ionic/app-scripts';
+const rootDir = process.env.IONIC_ROOT_DIR;
+const ionicEnv = recoverFailedIonicEnv(process.env.IONIC_ENV);
+
+var config = require(path.join(appScriptsDir, 'config', 'webpack.config.js'));
+const pathToEnvModule = resolvePathToEnvModule(ionicEnv);
+ 
 
 module.exports = function () {
-  var pathToEnvModule = resolvePathToEnvModule(process.env.IONIC_ENV);
-  console.log(`NODE_ENV : ${process.env.NODE_ENV}`)
-  console.log(`process.env.IONIC_ENV : ${process.env.IONIC_ENV}`);
-  console.log(`pathToEnvModule : ${pathToEnvModule}`);
   // set process.env as a global variable.
-  config[process.env.IONIC_ENV].plugins.push(
+  config[ionicEnv].plugins.push(
     new webpack.DefinePlugin({
       'process.env': JSON.stringify(process.env)
     })
   );
-  // set the pat of the alias @environment to the cur env. 
-  config[process.env.IONIC_ENV].resolve.alias = {
-      "@environment": pathToEnvModule
+  // set the path of the alias @environment to the cur env. 
+  config[ionicEnv].resolve.alias = {
+      "@environment": pathToEnvModule,
   };
-  config[process.env.IONIC_ENV].resolve.extensions = ['.ts', '.js', '.json'];
+  config[ionicEnv].resolve.extensions = ['.ts', '.js', '.json'];
   
   return config;
 }
